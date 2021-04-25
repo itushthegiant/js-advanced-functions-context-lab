bffc46a5a46b5ec280f914bf8002a828fa152c58
etamar <etamar.levy@gmail.com>
2021-04-25 16:01:10 -0400

    commit tree a91191da2c08236635850b2796e294e8b7b54e3f

--- index.js
+++ index.js
@@ -9,6 +9,72 @@
  for you to use if you need it!
  */
 
+
+ function createEmployeeRecord(array) {
+    let testEmployee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return testEmployee;
+}
+
+function createEmployeeRecords(rowData) {
+    return rowData.map(array => {
+     return createEmployeeRecord(array)
+    })
+ }
+
+ function createTimeInEvent(date) {
+     const timeIn = date.split(" ");
+     let newEvent = {
+        type: 'TimeIn',
+        hour: parseInt(timeIn[1]),
+        date: timeIn[0]
+    }
+    this.timeInEvents.push(newEvent)
+    return this;
+ }
+
+
+ function createTimeOutEvent(date) {
+    const timeOut = date.split(" ");
+    let newEvent = {
+       type: 'TimeOut',
+       hour: parseInt(timeOut[1]),
+       date: timeOut[0]
+   }
+   this.timeOutEvents.push(newEvent)
+   return this;
+}
+
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(day => date == day.date);
+    const timeOut = this.timeOutEvents.find(day => date == day.date);
+    const totalHours = (timeOut.hour - timeIn.hour) / 100;
+    return totalHours;
+}
+
+
+function wagesEarnedOnDate(date) {
+    const payRate = this.payPerHour;
+    const hours = hoursWorkedOnDate.call(this, date);
+    const total = hours * payRate;
+    return total
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    firstName = srcArray.find(fn => fn.firstName)
+    return firstName;
+}
+
+
+
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +85,13 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+
+
+
+function calculatePayroll(array) {
+    return array.reduce((acc, val) => {
+        return acc + allWagesFor.apply(val)
+    }, 0)
 }
